f98cdc94ec6b93d97e01d2ccce098367b0577926
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-11-17 16:04:31 -0300

    Add reminder ecto.model

---
+++ priv/repo/migrations/20191117190409_create_reminders.exs
@@ -0,0 +1,13 @@
+defmodule StudyReminder.Repo.Migrations.CreateReminders do
+  use Ecto.Migration
+
+  def change do
+    create table(:reminders) do
+      add :name, :string
+      add :reminder_enabled, :boolean, default: false, null: false
+
+      timestamps()
+    end
+
+  end
+end
