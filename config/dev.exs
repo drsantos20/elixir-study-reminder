3c12c83ddb201ec426049567094398c8a6660283
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-11-17 16:11:20 -0300

    Add postgres from docker

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :study_reminder, StudyReminder.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "dev",
+  password: "dev123",
   database: "study_reminder_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
