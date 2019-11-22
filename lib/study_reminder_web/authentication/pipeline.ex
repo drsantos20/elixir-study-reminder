1e96367283c50f081146612948ac4e1a432b796a
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-11-21 23:51:39 -0300

    Add authentication pipeline on routers

---
+++ lib/study_reminder_web/authentication/pipeline.ex
@@ -0,0 +1,9 @@
+defmodule StudyReminderWeb.Authentication.Pipeline do
+    use Guardian.Plug.Pipeline, otp_app: :busi_api,
+      module: StudyReminderWeb.Authentication.Guardian,
+      error_handler: StudyReminderWeb.Authentication.ErrorHandler
+  
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.EnsureAuthenticated
+    plug Guardian.Plug.LoadResource
+  end
